2f967c4a219ed8ad3e202ebabc03c057497ff7eb
Pradip <Pradip@Pihu>
2019-06-09 17:17:05 +0530

    SpringBootRunnerWithOrder

---
+++ src/main/java/com/app/runner/AppBRunner.java
@@ -0,0 +1,18 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class AppBRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+         System.out.println("From Runner-B");
+
+	}
+
+}
